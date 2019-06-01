c46c428abd3a70fcf40fed378221b64254340b8f
KatarzynaPaszko <pkatarzynapaszko@gmail.com>
2019-06-01 21:29:36 +0200

    db json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ContactComponent } from './contact/contact.component';
+import { BasketComponent } from './basket/basket.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'productsList'},
+  { path: 'products', component: ProductsListComponent},
+  { path: 'product/:id', component: ProductDetailsComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'basket', component: BasketComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasketComponent } from './basket/basket.component';
+import { CantactComponent } from './cantact/cantact.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasketComponent,
+    CantactComponent,
+    ContactComponent,
+    ProductsComponent,
+    ProductsListComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
